ade1432492b2487fcdc5bda70c2b301fedf8bc16
EC2 Default User <ec2-user@ip-172-31-14-211.ap-southeast-1.compute.internal>
2018-01-25 09:03:21 +0000

    char

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -18,9 +18,9 @@ class WebhookController < ApplicationController
 
     case event_type
     when "message"
-      # input_text = event["message"]["text"]
+      input_text = event["message"]["text"]
       # output_text = input_text
-      output_text = "チャー"
+      output_text = input_text + "チャー"
     end
 
     client = LineClient.new(CHANNEL_SECRET, CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
