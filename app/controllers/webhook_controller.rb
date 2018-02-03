33c7be44e484a7556f5c1824f0a6ba2c9cd9a286
岩田裕登 <jyuchan415@gmail.com>
2018-02-03 09:19:41 +0900

    :ok

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,6 +4,9 @@ class WebhookController < ApplicationController
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
+  @@behind_text = "チャー"
+  @@masa_array = []
+
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
@@ -26,9 +29,24 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+          input_text = event.message['text']
+          if input_text == "change-to-char" then
+            @@masa_array.delete(userId)
+            output_text = "チャーに切替"
+          elsif input_text == "change-to-masa" then
+            @@masa_array << userId
+            output_text = "まさに切替"
+          else
+            
+            if @@masa_array.include?(userId) then
+              output_text = input_text + "まさ"
+            else
+              output_text = input_text + "チャー"
+            end
+          end
           message = {
             type: 'text',
-            text: event.message['text']
+            text:  output_text
           }
           client.reply_message(event['replyToken'], message)
         end
