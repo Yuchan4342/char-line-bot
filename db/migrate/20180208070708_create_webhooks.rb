d9db35b7429eb41019239c197d3aa552e0ef5eaf
岩田裕登 <jyuchan415@gmail.com>
2018-02-08 16:08:31 +0900

    Added model webhook

---
+++ db/migrate/20180208070708_create_webhooks.rb
@@ -0,0 +1,10 @@
+class CreateWebhooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :webhooks do |t|
+      t.string :type
+      t.string :user_id
+
+      t.timestamps
+    end
+  end
+end
