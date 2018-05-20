a356d8abd7f5be98a54ff3bae2b026f650b9dcdb
岩田裕登 <jyuchan415@gmail.com>
2018-05-20 12:44:56 +0900

    webhook/index.html.erb とその routing の削除.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/callback' => 'webhook#callback'
-
   root 'top#index'
 
   get 'top/index'
   get 'top' => 'top#index'
 
-  get 'webhook/index'
-  get 'webhook' => 'webhook#index'
-
+  post '/callback' => 'webhook#callback'
 end
