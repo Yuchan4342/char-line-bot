97db6d6e3bcca0d16e144541277c0f30e132a1ae
岩田裕登 <jyuchan415@gmail.com>
2018-05-20 12:42:36 +0900

    Controller Top を追加.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'webhook/index'
-  root 'webhook#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/callback' => 'webhook#callback'
+
+  root 'top#index'
+
+  get 'top/index'
+  get 'top' => 'top#index'
+
+  get 'webhook/index'
+  get 'webhook' => 'webhook#index'
+
 end
