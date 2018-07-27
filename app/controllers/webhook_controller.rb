19179619d8625f584b66fb1ad8f6772e8a31ff5f
Yuto Iwata <jyuchan415@gmail.com>
2018-07-27 23:03:27 +0900

    rubocop 対応.

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,49 +3,49 @@
 # WebhookController
 # LINE からのリクエストに答えるコントローラ
 class WebhookController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
+  require 'line/bot' # gem 'line-bot-api'
 
   # callbackアクションのCSRFトークン認証を無効
-  protect_from_forgery :except => [:callback]
+  protect_from_forgery except: [:callback]
 
-  # リッチメニューのID設定 
+  # リッチメニューのID設定
   RICHMENU_ID = ENV['RICHMENU_ID']
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
+    @client ||= Line::Bot::Client.new do |config|
       # シークレットとアクセストークンの設定
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
-  # 送信ユーザとリッチメニューをリンクする
-  def link_menu (user_id)
-    if User.find_by(user_id: user_id).linked
-      uri = URI.parse("https://api.line.me/v2/bot/user/#{user_id}/richmenu/#{RICHMENU_ID}")
-      header = {'Authorization': "Bearer #{client.channel_token}"}
-
-      req = Net::HTTP::Post.new(uri.path, header)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      
-      res = http.start do |h|
-        h.request(req)
-      end
-      puts "Linked. #{res.code} #{res.body}"
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
     end
   end
 
-  def unlink_menu (user_id)
+  # 送信ユーザとリッチメニューをリンクする
+  def link_menu(user_id)
+    return unless User.find_by(user_id: user_id).linked
+    uri = URI.parse("https://api.line.me/v2/bot/user/#{user_id}/richmenu/#{RICHMENU_ID}")
+    header = { 'Authorization': "Bearer #{client.channel_token}" }
+
+    req = Net::HTTP::Post.new(uri.path, header)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    res = http.start do |h|
+      h.request(req)
+    end
+    puts "Linked. #{res.code} #{res.body}"
+  end
+
+  def unlink_menu(user_id)
+    return if User.find_by(user_id: user_id).linked
     uri = URI.parse("https://api.line.me/v2/bot/user/#{user_id}/richmenu")
-    header = {'Authorization': "Bearer #{client.channel_token}"}
+    header = { 'Authorization': "Bearer #{client.channel_token}" }
 
     req = Net::HTTP::Delete.new(uri.path, header)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-      
-    res = http.start do |http|
-      http.request(req)
+
+    res = http.start do |h|
+      h.request(req)
     end
     puts "Link deleted. #{res.code} #{res.body}"
   end
@@ -72,7 +72,12 @@ class WebhookController < ApplicationController
       else
         p 'create new User'
         # ユーザIDをデータベースに追加する
-        User.create(talk_type: event['source']['type'], user_id: user_id, masa: false, linked: true)
+        User.create(
+          talk_type: event['source']['type'],
+          user_id: user_id,
+          masa: false,
+          linked: true
+        )
       end
 
       case event
@@ -98,13 +103,13 @@ class WebhookController < ApplicationController
               output_text = 'リッチメニューを追加しました。\n削除したいときは「メニュー削除」と送ってください。'
             end
           elsif input_text == 'メニュー削除'
-            unless user.linked
-              output_text = 'リッチメニューはすでに削除されています。'
-            else
+            if user.linked
               # リッチメニューとのリンクを削除する
               user.update(linked: false)
               unlink_menu(user_id)
               output_text = "リッチメニューを削除しました。\n追加したいときは「メニュー追加」と送ってください。"
+            else
+              output_text = 'リッチメニューはすでに削除されています。'
             end
           else
             output_text = input_text + (user.masa ? 'まさ' : 'チャー')
