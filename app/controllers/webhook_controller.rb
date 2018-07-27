1f17cd145a5c66d19015adc899f15b254602a8a3
Yuto Iwata <jyuchan415@gmail.com>
2018-07-27 22:21:08 +0900

    frozen_string_literal: true を追加.

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # WebhookController
 # LINE からのリクエストに答えるコントローラ
 class WebhookController < ApplicationController
--- app/helpers/webhook_helper.rb
+++ app/helpers/webhook_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # WebhookHelper
 module WebhookHelper
 end
