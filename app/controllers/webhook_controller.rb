0d3ddaca279185ce2fefde204ba744b987117c8e
Yuto Iwata <jyuchan415@gmail.com>
2018-07-28 13:23:24 +0900

    微修正.

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -135,8 +135,8 @@ class WebhookController < ApplicationController
   def link_menu_request(uri)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    req = Net::HTTP::Delete.new(uri.path, header)
     header = { 'Authorization': "Bearer #{client.channel_token}" }
+    req = Net::HTTP::Delete.new(uri.path, header)
     http.start do |h|
       h.request(req)
     end
