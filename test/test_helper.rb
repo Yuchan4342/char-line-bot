00d6c0b6e544815f3a4c4dc9011211ec1298d975
Yuto Iwata <jyuchan415@gmail.com>
2018-07-28 23:12:45 +0900

    test_helper.rb の修正.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-require File.expand_path('../../config/environment', __dir__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
-class ActiveSupport
-  class TestCase
-    # Setup all fixtures in test/fixtures/*.yml
-    # for all tests in alphabetical order.
-    fixtures :all
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml
+  # for all tests in alphabetical order.
+  fixtures :all
 
-    # Add more helper methods to be used by all tests here...
-  end
+  # Add more helper methods to be used by all tests here...
 end
