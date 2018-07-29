c964b4fd3d3171e6d864544c7dab3ae0f8899339
Yuto Iwata <jyuchan415@gmail.com>
2018-07-29 12:47:02 +0900

    テストケース準備中.

--- test/controllers/webhook_controller_test.rb
+++ test/controllers/webhook_controller_test.rb
@@ -3,7 +3,44 @@
 require 'test_helper'
 
 class WebhookControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'post callback' do
+    json = {
+      'events': [
+        {
+          'replyToken': '0f3779fba3b349968c5d07db31eab56f',
+          'type': 'message',
+          'timestamp': 1_462_629_479_859,
+          'source': {
+            'type': 'user',
+            'userId': 'hoge1'
+          },
+          'message': {
+            'id': '325708',
+            'type': 'text',
+            'text': 'Hello, world'
+          }
+        },
+        {
+          'replyToken': '8cf9239d56244f4197887e939187e19e',
+          'type': 'message',
+          'timestamp': 1_462_629_479_859,
+          'source': {
+            'type': 'user',
+            'userId': 'hoge1'
+          },
+          'message': {
+            'id': '325709',
+            'type': 'text',
+            'text': 'HogeHoge'
+          }
+        }
+      ]
+    }.to_json
+    # uri = URI.parse('http://localhost:3000/callback')
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # req = Net::HTTP::Post.new(uri.path)
+    # req.body = json
+    # res = http.request(req)
+    # assert_equal('200', res.code)
+  end
 end
