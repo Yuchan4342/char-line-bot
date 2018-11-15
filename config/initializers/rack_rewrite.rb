913446656d44a9cc98de15ebced72b77e359000d
Yuto Iwata <jyuchan415@gmail.com>
2018-11-16 08:48:53 +0900

    TODO 追加.

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -2,7 +2,7 @@
 
 if ENV['RACK_ENV'] == 'production'
   LineBot::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    # /callback のみ除外
+    # /callback のみ除外(TODO: 新ドメインの HTTPS 対応したら除外をなくす)
     r301 %r{.*(?<!callback)$}, 'http://linebot.o-char.com$&', if: proc { |rack_env|
       rack_env['SERVER_NAME'] != 'linebot.o-char.com'
     }
