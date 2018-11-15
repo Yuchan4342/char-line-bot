58b526622422f9346a606e30463fbca7b53cd3b5
Yuto Iwata <jyuchan415@gmail.com>
2018-11-16 07:19:13 +0900

    リダイレクトを実装. ref: https://qiita.com/kon_yu/items/223aa03554ff6141001f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@
 # Application Controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :ensure_domain
+
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+
+    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める.
+    port = ":#{request.port}" unless [80, 443].include?(request.port)
+    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}",
+                status: :moved_permane
+  end
 end
