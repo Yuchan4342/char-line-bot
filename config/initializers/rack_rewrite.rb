8da70fd83a844c2639b022d3539750f5bd84874d
Yuto Iwata <jyuchan415@gmail.com>
2018-11-16 07:50:19 +0900

    リダイレクトの条件を微調整.

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -3,7 +3,7 @@
 if ENV['RACK_ENV'] == 'production'
   LineBot::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
     r301 /%r{.*}/, 'http://linebot.o-char.com$&', if: proc { |rack_env|
-      rack_env['SERVER_NAME'] == 'char-line-bot.herokuapp.com'
+      rack_env['SERVER_NAME'] != 'linebot.o-char.com'
     }
   end
 end
