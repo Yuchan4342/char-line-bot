6ba9e9a9ececdec1da3d7ba1351e6de9a02ba014
Yuto Iwata <jyuchan415@gmail.com>
2018-11-16 07:26:36 +0900

    リダイレクト先のURLを追加.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :ensure_domain
 
+  FQDN = 'linebot.o-char.com'
+
   def ensure_domain
     return unless /\.herokuapp.com/ =~ request.host
 
