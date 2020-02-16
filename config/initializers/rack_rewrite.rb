ff28be63f5a3297bb163e63962b396474f356df1
Yuto Iwata <jyuchan415@gmail.com>
2020-02-16 22:01:45 +0900

    本番向け heroku ドメインでのみ 301リダイレクトを行うようにした.

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 # For 301 redirect.
 
 if ENV['RACK_ENV'] == 'production'
   LineBot::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
     # 新 URL に redirect する.
     r301 %r{.*}, 'https://linebot.o-char.com$&', if: proc { |rack_env|
-      rack_env['SERVER_NAME'] != 'linebot.o-char.com'
+      # rack_env['SERVER_NAME'] != 'linebot.o-char.com' &&
+      rack_env['SERVER_NAME'] == 'char-line-bot.herokuapp.com'
     }
   end
 end
